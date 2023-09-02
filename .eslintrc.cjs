36b2f60143ff9fbe430f18ba5c24d4510211b7a2
Jakub Kolebaba <jakub@kolebaba.cz>
2023-09-02 10:21:18 +0200

    Added tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,46 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  env: {
+    es2020: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'unused-imports'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   rules: {
-    'react-refresh/only-export-components': [
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-var-requires': 1,
+    '@typescript-eslint/ban-types': [1, { types: { object: false } }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    // 'no-console': 'error',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
       'warn',
-      { allowConstantExport: true },
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        trailingComma: 'all',
+        singleQuote: true,
+        endOfLine: 'lf',
+        semi: true,
+        tabWidth: 2,
+      },
     ],
   },
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,57 @@
-import './App.css'
+import { configureChains, createConfig, disconnect, getAccount, mainnet } from '@wagmi/core';
+import { connect, watchAccount } from '@wagmi/core';
+import { InjectedConnector } from '@wagmi/core/connectors/injected';
+import { publicProvider } from '@wagmi/core/providers/public';
+import { useEffect, useState } from 'react';
 
-import {
-  createConfig,
-  configureChains,
-  mainnet, getAccount, disconnect,
-} from '@wagmi/core'
-import { publicProvider } from '@wagmi/core/providers/public'
-import { connect, fetchEnsName } from '@wagmi/core'
-import { InjectedConnector } from '@wagmi/core/connectors/injected'
+import { BlockNumber } from './components/BlockNumber.tsx';
 
-const injectedConnector = new InjectedConnector()
+const injectedConnector = new InjectedConnector();
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet],
-  [publicProvider()],
-)
+const { publicClient, webSocketPublicClient } = configureChains([mainnet], [publicProvider()]);
 
-const config = createConfig({
+createConfig({
   autoConnect: true,
   connectors: [injectedConnector],
   publicClient,
   webSocketPublicClient,
-})
+});
 
 function App() {
-  const account = getAccount()
+  const [account, setAccount] = useState(getAccount());
+  const handleConnect = async () => connect({ connector: injectedConnector });
+  const handleDisconnect = async () => disconnect();
 
-  const handleConnect = async () => {
-    const { account } = await connect({
-      connector: injectedConnector
-    })
+  useEffect(() => {
+    const unwatch = watchAccount((account) => setAccount(account));
 
-    console.log(account)
-
-    // const ensName = await fetchEnsName({ address: account })
-  }
-
-  const handleDisconnect = async () => {
-    await disconnect()
-  }
-
+    return () => {
+      unwatch();
+    };
+  }, []);
 
   return (
-    <>
-      {account.isConnected ? <>
-        <div><strong>{account.address}</strong></div>
-        <div><br /><button onClick={handleDisconnect}>disconnect</button></div>
-      </> : <button onClick={handleConnect}>connect</button>}
-    </>
-  )
+    <div>
+      <div className="p-3 bg-gray-200 flex justify-end">
+        <BlockNumber />
+      </div>
+      <div className="w-full h-full flex justify-center items-center flex-col mt-3">
+        <div>{account.status}</div>
+        {account.isConnected ? (
+          <>
+            <div>{account.address}</div>
+            <button className="mt-3 border py-1 px-3 rounded" onClick={handleDisconnect}>
+              disconnect wallet
+            </button>
+          </>
+        ) : (
+          <button className="mt-3 border py-1 px-3 rounded" onClick={handleConnect}>
+            connect wallet
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
